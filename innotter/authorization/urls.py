fee5ab5b399af202c300e27dbb5af90457eb0307
Andrey Strongin <andrewvs0707@gmail.com>
2022-10-24 19:45:00 +0300

    Implement Sign in/up

--- innotter/authorization/urls.py
+++ innotter/authorization/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('authentication/token/', views.ObtainTokens.as_view(), name='tokens_obtain'),
     path('authentication/token/refresh/', views.TokenRefresh.as_view(), name='token_refresh'),
     path('authentication/token/verify/', views.TokenVerify.as_view(), name='token_verify'),
+    path('signin/', views.LoginView.as_view(), name='signin'),
+    path('signup/', views.RegisterView.as_view(), name='signup'),
 ]
 
 urlpatterns += router.urls
--- innotter/authorization/views.py
+++ innotter/authorization/views.py
@@ -1,14 +1,15 @@
 from datetime import datetime, timedelta, timezone
 
+from django.contrib.auth import login
 import dotenv
 import jwt
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_202_ACCEPTED, HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 from user.models import User
-from .serializers import TokenRefreshSerializer, TokenVerifySerializer, ObtainTokensSerializer
-
+from .serializers import TokenRefreshSerializer, TokenVerifySerializer, \
+    ObtainTokensSerializer, LoginSerializer, RegisterSerializer
 
 config = dotenv.dotenv_values(".env")
 SECRET_KEY = config["JWT_SECRET_KEY"]
@@ -19,36 +20,34 @@ REFRESH_TOKEN_LIFE_TIME = config["REFRESH_TOKEN_LIFE_TIME"]
 
 class ObtainTokens(APIView):
     """
-    Take user's credentials. If the serialized creds are valid, return access and refresh tokens.
+    Take user's credentials. If the deserialized creds are valid, return access and refresh tokens.
     """
     serializer_class = ObtainTokensSerializer
 
     def post(self, request):
+        user_id = request.user.id
+
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user_id = request.user.id
+        serializer.is_valid(raise_exception=True)
 
-            access_token = get_access_token(user_id)
+        access_token = get_access_token(user_id)
 
-            refresh_payload = {"user_id": user_id,
-                               "iss": "innotter",
-                               "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=int(REFRESH_TOKEN_LIFE_TIME))}
-            refresh_token = jwt.encode(refresh_payload, SECRET_KEY, algorithm=SIGNING_METHOD)
+        refresh_payload = {"user_id": user_id,
+                           "iss": "innotter",
+                           "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=int(REFRESH_TOKEN_LIFE_TIME))}
+        refresh_token = jwt.encode(refresh_payload, SECRET_KEY, algorithm=SIGNING_METHOD)
 
-            User.objects.filter(pk=user_id).update(refresh_token=refresh_token)
+        User.objects.filter(pk=user_id).update(refresh_token=refresh_token)
 
-            data = {'info': '', 'access': access_token, 'refresh': refresh_token}
-            status = HTTP_200_OK
-        else:
-            data = {'info': 'Invalid data', 'access': '', 'refresh': ''}
-            status = HTTP_400_BAD_REQUEST
+        data = {'info': '', 'access': access_token, 'refresh': refresh_token}
+        status = HTTP_200_OK
 
         return Response(data, status=status)
 
 
 class TokenRefresh(APIView):
     """
-    Take refresh token. If the given serialized token is valid, return valid access token.
+    Take refresh token. If the given deserialized token is valid, return valid access token.
     """
     serializer_class = TokenRefreshSerializer
 
@@ -56,18 +55,16 @@ class TokenRefresh(APIView):
         user_id = request.user.id
 
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            refresh_token = serializer.validated_data['refresh_token']
-            _, status = token_verify(refresh_token)
-            if status == HTTP_200_OK:
-                access_token = get_access_token(user_id)
-                data = {'info': '', 'access': access_token}
-                status = HTTP_200_OK
-            else:
-                data = {'info': 'Invalid token', 'access': ''}
-                status = HTTP_400_BAD_REQUEST
+        serializer.is_valid(raise_exception=True)
+
+        refresh_token = serializer.validated_data['refresh_token']
+        _, status = token_verify(refresh_token)
+        if status == HTTP_200_OK:
+            access_token = get_access_token(user_id)
+            data = {'info': '', 'access': access_token}
+            status = HTTP_200_OK
         else:
-            data = {'info': 'Invalid data', 'access': ''}
+            data = {'info': 'Invalid token', 'access': ''}
             status = HTTP_400_BAD_REQUEST
 
         return Response(data, status=status)
@@ -75,21 +72,51 @@ class TokenRefresh(APIView):
 
 class TokenVerify(APIView):
     """
-    Take access token. Return whether the serialized token is valid or not.
+    Take access token. Return whether the deserialized token is valid or not.
     """
     serializer_class = TokenVerifySerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            refresh_token = serializer.validated_data['access_token']
-            data, status = token_verify(refresh_token)
+        serializer.is_valid(raise_exception=True)
 
-        else:
-            data = {'info': 'Invalid data'}
-            status = HTTP_400_BAD_REQUEST
+        refresh_token = serializer.validated_data['access_token']
+        data, status = token_verify(refresh_token)
 
         return Response(data, status=status)
+
+
+class LoginView(APIView):
+    """
+    Implement user login, return status=202.
+    """
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=HTTP_202_ACCEPTED)
+
+
+class RegisterView(APIView):
+    """
+    Implement user registration, return status=202.
+    """
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username, password, email = serializer.validated_data['username'], \
+                                    serializer.validated_data['password'], serializer.validated_data['email']
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+
+        return Response(None, status=HTTP_202_ACCEPTED)
 
 
 def get_access_token(user_id):
