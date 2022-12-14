cd0263c483ac0d66c6d4abf64a4e0fc3fea4d528
Andrey Strongin <andrewvs0707@gmail.com>
2022-12-14 21:40:02 +0300

    Added Fixtures

--- microservice/core/tests/tests_auth/tests_auth.py
+++ microservice/core/tests/tests_auth/tests_auth.py
@@ -12,26 +12,22 @@ client = TestClient(app)
 
 
 class TestAuthService:
-    _invalid_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJpc3MiOiJpbm5vdH' \
-                     'RlciIsImV4cCI6MTY3MDg4MzM1M30.G71aEzCS32bkVZswMK_czZlxh5xAV2GHIP7WWfy9Ovg'
-    _valid_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJpc3MiOiJpbm5vdH' \
-                   'RlciIsImV4cCI6ODgwNzA3OTczMDR9.h4Aq9b9qUAaGuJfV-BvQjPIlLfvVs8XNoNDOC3PXUYA'
     _url = '/api/v1/stats/1/'
 
-    def test_auth_service_invalid_token(self):
-        response = AuthService.verify_user_token(self._invalid_token)
+    def test_auth_service_invalid_token(self, create_invalid_token):
+        response = AuthService.verify_user_token(create_invalid_token)
         assert not response
 
-    def test_auth_service_valid_token(self):
-        response = AuthService.verify_user_token(self._valid_token)
+    def test_auth_service_valid_token(self, create_valid_token):
+        response = AuthService.verify_user_token(create_valid_token)
         assert response
 
-    def test_auth_service_403_response(self):
-        headers = {'Authorization': f'Bearer {self._invalid_token}'}
+    def test_auth_service_403_response(self, create_invalid_token):
+        headers = {'Authorization': f'Bearer {create_invalid_token}'}
         response = client.get(self._url, headers=headers)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_auth_service_200_response(self):
-        headers = {'Authorization': f'Bearer {self._valid_token}'}
+    def test_auth_service_200_response(self, create_valid_token):
+        headers = {'Authorization': f'Bearer {create_valid_token}'}
         response = client.get(self._url, headers=headers)
         assert response.status_code == status.HTTP_200_OK
